029c188ad3ff19111fc696f537ff078607add6f6
Matěj Šmíd <smidm@cmp.felk.cvut.cz>
2017-01-07 15:51:19 +0100

    flask rewrite work in progress

--- mendeley_add_citations.py
+++ mendeley_add_citations.py
@@ -8,18 +8,20 @@
 
 import sys
 sys.path.insert(0, './scholar')
-sys.path.insert(0, './mendeley-oapi-example')
 
-from mendeley_client import *
+from mendeley import Mendeley
+from mendeley.session import MendeleySession
+from flask import Flask, redirect, render_template, request, session
 from scholar import ScholarQuerier, SearchScholarQuery
 import re
 import time
 import difflib
 from random import randint
 import urllib2
+import yaml
 
 # skip documents already processed
-skip_documents = True
+skip_documents = False
 
 # min and max sleep time in seconds between Scholar requests
 min_sleep_time_sec = 10
@@ -83,6 +85,8 @@ def update_tags(oldtags, newtags):
 
 
 def has_citation_tag(tags, patterns):
+    if not tags:
+        return False
     for pattern in patterns:
         for t in tags:
             if re.match(pattern, t):
@@ -91,66 +95,119 @@ def has_citation_tag(tags, patterns):
     return False
 
 
-def process_document(document_id, skip_documents=False):
-    document = mendeley.document_details(document_id)
-    if skip_documents and has_citation_tag(document['tags'], tag_pattern):
-        return False
-
-    try:
-        query = SearchScholarQuery()
-        query.set_phrase(document['title'])
-        scholar.send_query(query)
-        scholar_articles = scholar.articles
-        if len(scholar_articles) == 0:
-            print('No scholar articles found for ' + document['title'])
-            return True
-    except urllib2.HTTPError as e:
-        print e.msg
-        print e.reason
-        sys.exit(-1)
+def process(document):        
+    scholar = ScholarQuerier() 
+    query = SearchScholarQuery()
+    query.set_phrase(document.title)
+    scholar.send_query(query)
+    scholar_articles = scholar.articles
+    if len(scholar_articles) == 0:
+        # print('No scholar articles found for ' + document.title)
+        return None
 
     if 'year' in document:
-        year = document['year']
+        year = document.year
     else:
         year = -1
 
-    print('%s\t\t%s\tScholar: %s' %
-         (scholar_articles[0]['num_citations'], year, scholar_articles[0]['title']))
-    print('\t\t\tMendeley: %s' % document['title'])
+    # print('%s\t\t%s\tScholar: %s' %
+    #     (scholar_articles[0]['num_citations'], year, scholar_articles[0]['title']))
+    #print('\t\t\tMendeley: %s' % document.title)
     title_match_ratio = \
-        difflib.SequenceMatcher(None, document['title'], scholar_articles[0]['title']).ratio()
+        difflib.SequenceMatcher(None, document.title, scholar_articles[0]['title']).ratio()
     if title_match_ratio < min_title_match_ratio:
-        print('Paper titles differ too much, skipping (match ratio: %f).' % title_match_ratio)
-    time.sleep(randint(min_sleep_time_sec, max_sleep_time_sec))
+        return None
+        # print('Paper titles differ too much, skipping (match ratio: %f).' % title_match_ratio)
+    # time.sleep(randint(min_sleep_time_sec, max_sleep_time_sec))
 
-    if not (title_match_ratio < min_title_match_ratio):
-        old_tags = document['tags']
-        citation_tag = ncitations_to_tag(scholar_articles[0]['num_citations'])
-        new_tags = update_tags(old_tags, [(tag_pattern, citation_tag)])
-        doc_updated = mendeley.update_document(docid, document={'tags': new_tags})
-        # print doc_updated
-    return True
+    old_tags = document.tags
+    citation_tag = ncitations_to_tag(scholar_articles[0]['num_citations'])
+    new_tags = update_tags(old_tags, [(tag_pattern, citation_tag)])
+    document.update(tags=new_tags)
+    
+    return scholar_articles[0]['num_citations']
+
+    
+# edit config.yml first
+with open('config.yml') as f:
+    config = yaml.load(f)
+
+REDIRECT_URI = 'http://localhost:5000/oauth'
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = config['clientSecret']
+
+mendeley = Mendeley(config['clientId'], config['clientSecret'], REDIRECT_URI)
+    
+@app.route('/')
+def home():
+    
+    print session.keys()
+    if 'token' in session:
+        return redirect('/addCitations')
+        
+    auth = mendeley.start_authorization_code_flow()
+    print auth.state
+    session['state'] = auth.state
+
+    return render_template('home.html', login_url=(auth.get_login_url()))
 
 
-# edit config.json first
-mendeley = create_client()
+@app.route('/oauth')
+def auth_return():   
+    auth = mendeley.start_authorization_code_flow(state=session['state'])
+    mendeley_session = auth.authenticate(request.url)
 
-if skip_documents:
-    print('Already tagged documents by mendeley_add_citations.py are skipped.')
-else:
-    print('Processing all documents, including already tagged by mendeley_add_citations.py.')
-print('See skip_documents variable in mendeley_add_citations.py to change this.\n')
-print('Tags are added immediately. You can interrupt the script and continue later.\n')
+    session.clear()
+    session['token'] = mendeley_session.token
 
-print('citations\tyear\ttitle')
+    return redirect('/addCitations')
 
-num_skipped = 0
-documents = mendeley.library(page=0, items=items_per_request)
-for page_number in xrange(documents['total_pages']):
-    documents = mendeley.library(page=page_number, items=items_per_request)
-    scholar = ScholarQuerier()
-    for docid in documents['document_ids']:
-        if not process_document(docid, skip_documents):
-            num_skipped += 1
+@app.route('/addCitations')
+def add_citations():
+    if 'token' not in session:
+        return redirect('/')
+        
+    mendeley_session = get_session_from_cookies()
+    
+    print session
 
-print(str(num_skipped) + ' already processed documents skipped, see skip_documents variable.')
+    if 'process_id' not in session or 'ids' not in session:
+        session['process_id'] = 0
+        session['ids'] = [doc.id for doc in mendeley_session.documents.iter()]
+        # return redirect('/addCitations')
+        
+    document = mendeley_session.documents.get(session['ids'][session['process_id']], view='tags')
+    session['process_id'] += 1
+    error = None
+    num_citations = -1
+    if skip_documents and has_citation_tag(document.tags, tag_pattern):
+        error = 'skipped'
+    else:  
+        try:
+            num_citations = process(document)
+        except urllib2.HTTPError as e:
+            if e.code == 503:
+                error = 'blocked'
+            else:
+                error = 'http_error'          
+        
+    # time.sleep(randint(min_sleep_time_sec, max_sleep_time_sec))    
+    
+    return render_template('add_citations.html', num_citations=num_citations, error=error, title=document.title)
+ 
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    return redirect('/')
+
+
+def get_session_from_cookies():
+    return MendeleySession(mendeley, session['token'])
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'   
+        
+if __name__ == '__main__':
+    app.run()
+
